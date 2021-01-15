14c193e8c08bfd2c1fccbbed4a5e7b265c0fbc1a
Javier <jsuarezmartin1982@gmail.com>
2021-01-15 22:53:48 +0100

    Conversor HEX-RGB

---
+++ php/color.php
@@ -0,0 +1,89 @@
+<?php
+    class Color {
+
+        // Atributos privados (encapsulamiento)
+        private $color = array(
+                                "r" => "", 
+                                "g" => "", 
+                                "b" => ""
+                            );        
+
+        // Constructores
+
+        public function __construct() {
+            $this->color["r"] = "";
+            $this->color["g"] = "";
+            $this->color["b"] = "";
+        }
+
+        // Métodos Get.
+
+        public function getColor() {
+            return $this->color;
+        }
+               
+        // Métodos Set.
+
+        public function setColorRed($r) {
+            $this->color["r"] = $r;
+        }
+
+        public function setColorGreen($g) {
+            $this->color["g"] = $g;
+        }
+
+        public function setColorBlue($b) {
+            $this->color["b"] = $b;
+        }
+
+        // Otras funciones.
+
+        public function hexToRgb($color_hex) {
+
+            // Comprobamos que el color está en el formato hexadecimal correcto.
+
+            if (strlen($color_hex) == 7) {
+                if(substr($color_hex, 0, 1) == "#") {
+
+                    // Comprobamos que sean valores válidos (Entre 00 y FF);
+                    if ($this->validarHex($color_hex)) {
+                        // Extraemos los valores separados y los asignamos.
+                        $this->setColorRed(hexdec(substr($color_hex, 1, 2)));
+                        $this->setColorGreen(hexdec(substr($color_hex, 3, 2)));
+                        $this->setColorBlue(hexdec(substr($color_hex, 5, 2)));
+                        return $this->color; 
+                    } else {
+                        return("ERROR");
+                    };
+                } else {
+                    return ("ERROR");
+                };
+            } else {
+                return ("ERROR");
+            };
+        }
+
+
+        public function validarHex($valor_hex) {
+            $valido = true;
+            $valor_validar = substr($valor_hex, 1);
+            for($i=0; $i<strlen($valor_validar); $i++) {
+                if ($valor_validar[$i] >= "0" && $valor_validar[$i] <= "9") {
+                    $valido = true;
+                } else if ($valor_validar[$i] >= "A" && $valor_validar[$i] <= "F") {                    
+                    $valido = true;
+                } else {
+                    $valido = false;
+                };
+                
+                // Si encontramos un valor no válido forzamos salida del bucle.
+                if ($valido == false){
+                    $i=strlen($valor_validar);
+                };
+            };
+            
+            return $valido;
+        }
+
+    };
+?>
---
+++ php/hexrgb.php
@@ -0,0 +1,53 @@
+<?php
+    include('./color.php');
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Color HEX > RGB</title>
+    <link rel="stylesheet" href="../css/styles.css" type="text/css">
+</head>
+<body>
+    <div class="container">    
+        <?php        
+            // Comprobamos que se han enviado los datos del formulario.
+            if(!empty($_POST)) {
+                
+                // Comprobamos que todos los campos contienen algun valor.
+                if (isset($_POST["color"])) {
+
+                    // Almacenamos los datos recibidos en una variable para que sea mas sencillo manejarlo.
+                    $color_hex = strtoupper(trim($_POST["color"])); // Eliminamos espacios en blanco y convertimos en mayúsculas.
+                    $color_rgb =  new Color();
+
+                    // Realizamos comprobaciones de formato y convertimos a RGB.
+                    
+                    if($color_rgb->hexToRgb($color_hex) != "ERROR") {
+                       
+                       // Escribimos los datos
+                        echo ("<p> El color en formato RGB es:</p>");
+                        echo ("<p>rgb(");
+                        foreach($color_rgb->getColor() as $clave => $valor) {
+                            if ($clave == "r" || $clave =="g") {
+                                echo($valor);
+                                echo(", ");
+                            } else {
+                                echo($valor);
+                                echo(")</p>");
+                            };                            
+                        };              
+                        
+                    } else {
+                        echo ("<p> El color seleccionado no tiene el formato correcto (#RRGGBB)<p>");                       
+                    };
+
+                    echo ("<p><a href='../html/index.html'>Volver</a></p>");
+                };
+            };
+        ?>
+    </div>
+</body>
+</html>
+
